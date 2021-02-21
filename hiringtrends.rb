ac5461e619a8b0e61e59f44e0651b9e6d8dd7c58
Ryan W <rwilliamz@gmail.com>
2021-02-21 11:09:28 -0800

    Feb post

--- hiringtrends.rb
+++ hiringtrends.rb
@@ -1,9 +1,9 @@
 require_relative 'lib/hiringtrends'
 
-hn = HiringTrends::Program.new "https://gist.githubusercontent.com/ryanwi/6135845/raw/9aa9ee8359bc988beabe13ae246cd40a0448fdce/software-terms.dic"
+hn = HiringTrends::Program.new "https://gist.githubusercontent.com/ryanwi/6135845/raw/1b040e1d1aaad488ed353f09d0b4150dcb123c02/software-terms.dic"
 hn.clean
 hn.get_submissions
 hn.get_comments_for_submissions
 hn.save_submissions
 hn.analyze_submissions
-hn.publish("December", "2020", "Thursday", "31", 20)
+hn.publish("February", "2021", "Sunday", "21", 20)
