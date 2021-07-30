aba6904afe652d75969729ce4b9f4fed85d267c2
Ryan W <rwilliamz@gmail.com>
2021-07-30 14:32:23 -0700

    Add terms, new version

--- hiringtrends.rb
+++ hiringtrends.rb
@@ -1,6 +1,6 @@
 require_relative 'lib/hiringtrends'
 
-hn = HiringTrends::Program.new "https://gist.githubusercontent.com/ryanwi/6135845/raw/1b040e1d1aaad488ed353f09d0b4150dcb123c02/software-terms.dic"
+hn = HiringTrends::Program.new "https://gist.githubusercontent.com/ryanwi/6135845/raw/9c13f1e036b8f18554fccd4755c1cc3aeac00453/software-terms.dic"
 hn.clean
 hn.get_submissions
 hn.get_comments_for_submissions
