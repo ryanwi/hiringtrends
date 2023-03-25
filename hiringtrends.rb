aea3d99e2938ef23221c4ee3f7eadfcf2263e50b
Ryan W <rwilliamz@gmail.com>
2023-03-25 14:58:13 -0700

    New terms

--- hiringtrends.rb
+++ hiringtrends.rb
@@ -2,11 +2,11 @@
 
 require_relative 'lib/hiringtrends'
 
-terms = "https://gist.githubusercontent.com/ryanwi/6135845/raw/ec4683d6156a1f4158e105c242c72dfaf77320d4/software-terms.dic"
+terms = "https://gist.githubusercontent.com/ryanwi/6135845/raw/da40c9790227757fcc4f63a84be56e54215813a7/software-terms.dic"
 hn = HiringTrends::Program.new terms
 hn.clean
 hn.get_submissions
-hn.get_comments_for_submissions
-hn.save_submissions
-hn.analyze_submissions
-hn.publish("March", "2022", "Sunday", "20")
+hn.get_comments_for_submissions;nil
+hn.save_submissions;nil
+hn.analyze_submissions;nil
+hn.publish("March", "2023", "Saturday", "20")
