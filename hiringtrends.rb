3200df896e1abe843cfd60b4143de0094262ec8f
Ryan W <rwilliamz@gmail.com>
2023-05-21 17:04:07 -0700

    may draft

--- hiringtrends.rb
+++ hiringtrends.rb
@@ -9,4 +9,4 @@ hn.get_submissions
 hn.get_comments_for_submissions;nil
 hn.save_submissions;nil
 hn.analyze_submissions;nil
-hn.publish("April", "2023", "Sunday", "29")
+hn.publish("May", "2023", "Sunday", "21")
