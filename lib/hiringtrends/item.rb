e1ae998cc99a1fe697f76421d1d4463e6f9ba323
Ryan W <rwilliamz@gmail.com>
2023-09-16 23:43:23 -0700

    move analysis to item

--- lib/hiringtrends/item.rb
+++ lib/hiringtrends/item.rb
@@ -3,7 +3,7 @@
 module HiringTrends
   # Represents an individual hn item with associated details.
   class Item
-    attr_reader :id, :created_at, :source, :title, :author, :num_comments, :points
+    attr_reader :id, :created_at, :source, :title, :author, :num_comments, :points, :terms
     attr_accessor :comments
 
     def initialize(result)
@@ -24,8 +24,47 @@ module HiringTrends
       "/api/v1/items/#{id}"
     end
 
+    def analyze(comments, terms)
+      HiringTrends.logger.info "== Analyzing #{title} =="
+      @comments = comments
+      @terms = terms
+
+      count_terms_in_comments
+      calculate_percentage_for_terms
+      rank_terms_by_count
+
+      @terms
+    end
+
     private
 
     attr_writer :id, :created_at, :source, :title, :author, :num_comments, :points
+
+    # accumulate mentions of term as comments are searched
+    def count_terms_in_comments
+      comments.each do |comment|
+        comment_text = comment["text"]
+        next if comment_text.nil?
+
+        posting = HiringTrends::JobPosting.new(comment_text)
+
+        # identify if each term is in the comment
+        terms.each_key do |term|
+          # increment count as its found
+          terms[term][:count] += 1 if posting.term?(terms[term][:full_term])
+        end
+      end
+    end
+
+    def calculate_percentage_for_terms
+      terms.each_key do |term|
+        terms[term][:percentage] = ((terms[term][:count] / comments.count.to_f) * 100).round(2)
+      end
+    end
+
+    def rank_terms_by_count
+      ranked_terms = terms.sort_by { |_k, v| -v[:count] }.to_a
+      ranked_terms.each_with_index { |item, index| terms[item[0]][:rank] = index + 1 }
+    end
   end
 end
